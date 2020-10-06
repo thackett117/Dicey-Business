42760b83557aa7be7162809ca4dccb8a50be2af4
thackett117 <thackett117@gmail.com>
2020-10-06 10:06:02 -0500

    made dice appear and reroll with some buttons

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-let btnGenerate = document.getElementById('btnGenerate')
-btnGenerate.addEventListener('click', () => console.log('hi'))
+let btnGenerate = document.getElementById('btnGenerate');
+let diceContainer = document.getElementById('diceContainer');
+let btnRoll = document.getElementById('btnRoll');
+let diceArray = [];
+
+
+class Die {
+    constructor() {
+        this.div = document.createElement('div');
+        this.div.className = 'die';
+        this.roll();
+        diceContainer.appendChild(this.div);
+        diceArray.push(this);
+        
+    }
+
+    roll() {
+        this.value = randomDieNum(1, 6);
+        this.div.innerText = this.value;
+    }
+}
+
+
+btnGenerate.addEventListener('click', function() {
+    new Die();
+})
+
+btnRoll.addEventListener('click', () => {
+    diceArray.forEach(die => {
+        die.roll();
+    })
+})
+
+
+randomDieNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
